6083a0ec50fcbaaad78f14abc5614a579176e2c6
wankuan-VM <794964984@qq.com>
2020-03-28 02:32:00 +0800

    [NEW]add comments for single-connection

--- client.c
+++ client.c
@@ -8,27 +8,43 @@
 
 int main(int argc, char *argv[])
 {
-    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    /* 获得套接字描述符 */
+    int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(client_socket < 0){
+		perror("ERROR");
+		exit(1);
+    }
+    /* 套接字地址结构 */
     struct sockaddr_in serv_addr;
-
     memset(&serv_addr, 0, sizeof(serv_addr));
 
+    /* 本地的8000端口 */
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8000);
+    serv_addr.sin_port = htons(8888);
 
-    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
+    /* 连接服务器 */
+    if(connect(client_socket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		perror("ERROR");
+		exit(1);
+    }
+    /* 三条消息 */
     const char buffer[3][100]={"1st message","2nd message","3rd message"};
     int mesg_len = sizeof(buffer)/sizeof(buffer[0]);
 
+    /* 依次发送 */
     for(int i=0; i<mesg_len; i++){
-        write(sock, buffer[i], strlen(buffer[i]));
+        /* 分三次发送消息
+        ** 如发送过快，服务器接收的消息将会黏在一起，俗称“粘包”，粘包原因：
+        ** 1.连续发送数据时，tcp协议栈会将小内容拼接成大内容，一次性发送到服务器端，因此造成粘包
+        ** 2.当发送内容较大时，由于服务器端的recv（buffer_size）方法中的buffer_size较小，
+        ** 不能一次性完全接收全部内容，因此在下一次请求到达时，接收的内容依然是上一次没有完全接收完的内容
+        */
+        write(client_socket, buffer[i], strlen(buffer[i]));
+        printf("mesg:%s\n", buffer[i]);
         sleep(1);
     }
-    // if(shutdown(sock,SHUT_RDWR) != 0){
-    //     printf("error shutdown\n");
-    // }
-    close(sock);
+    /* 关闭socket，关闭TCP连接 */
+    close(client_socket);
     return 0;
 }
--- server.c
+++ server.c
@@ -12,52 +12,67 @@ int main(int argc , char *argv[])
 	struct sockaddr_in servaddr, cliaddr;
 	int listenfd, connfd;
 
-	socklen_t clilen;
-
+    /* 获得套接字描述符 */
 	if((listenfd = socket(AF_INET , SOCK_STREAM , 0)) < 0){
-		perror("socket error");
+		perror("ERROR");
 		exit(1);
 	}
 	memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(clilen));
-
+    memset(&cliaddr, 0, sizeof(cliaddr));
+    /* 接收指定端口的所有消息 */
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(8000);
+	servaddr.sin_port = htons(8888);
 
+    char *addr_s;
+    uint16_t port;
+    /* 将整形地址转为字符型 */
+    addr_s = inet_ntoa(servaddr.sin_addr);
+    /* 网络字节序转换 */
+    port = ntohs(servaddr.sin_port);
+    printf("local host:%s, port:%u\n",addr_s, port);
+
+    /* 将套接字与地址结构绑定 */
 	if(bind(listenfd , (struct sockaddr*)&servaddr , sizeof(servaddr)) < 0){
-		perror("bind error");
+		perror("ERROR");
+		exit(1);
+	}
+    /* 允许最大连接数1024 */
+    /* 将主动套接字转为被动套接字，指使内核可以接收该套接字的请求 */
+	if(listen(listenfd, 1024) < 0){
+		perror("ERROR");
 		exit(1);
 	}
 
-	if(listen(listenfd , 666) < 0){
-		perror("listen error");
-		exit(1);
-	}
-
-    clilen = sizeof(cliaddr);
-    if((connfd = accept(listenfd , (struct sockaddr *)&cliaddr , &clilen)) < 0 ){
+    uint16_t clilen = sizeof(cliaddr);
+    /* 返回已连接的套接字（已完成TCP三次握手），完成服务时，将会被关闭
+    ** 第一个参数为监听套接字，在整个服务器生命周期一直存在，仅一个，用于监听新TCP连接
+    ** 第二、三参数为已连接TCP请求的客户端信息，可设为空指针
+    */
+    if((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, (socklen_t*)&clilen)) < 0 ){
         perror("accept error");
         exit(1);
     }
 
-    char *addr_s = inet_ntoa(cliaddr.sin_addr);
-    uint16_t port = ntohs(cliaddr.sin_port);
+    addr_s = inet_ntoa(cliaddr.sin_addr);
+    port = ntohs(cliaddr.sin_port);
     printf("remote host:%s, port:%u\n",addr_s, port);
 
-    addr_s = inet_ntoa(servaddr.sin_addr);
-    port = ntohs(servaddr.sin_port);
-    printf("local host:%s, port:%u\n",addr_s, port);
 
     ssize_t n;
-    char buff[1000];
-
-    while((n = read(connfd, buff, 1000))>0)
+    char buf[1000];
+    /* 接收消息，read会一直阻塞，知道套接字接收到新消息或者被关闭，被关闭返回值为0
+    ** 如果接收缓冲区设置过小，下一次消息来临时，会造成“粘包”现象
+    */
+    while((n = read(connfd, buf, sizeof(buf)))>0)
     {
-        buff[n]='\0';
-        printf("mesg:%s\n", buff);
+        buf[n]='\0';
+        /* 接收消息，并打印出长度 */
+        printf("mesg:%s\n", buf);
         printf("len:%ld\n", n);
     }
+    /* 客户端已关闭，关闭已连接的套接字 */
     close(connfd);
+    /* 监听套接字关闭，服务器关闭 */
 	close(listenfd);
 }
